3137b3c10906d484dde91189be170ce5676c72f2
Alex P. Scigalszky <alexpablo.90@gmail.com>
2022-11-22 15:01:50 -0300

    use correct key words

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-import { data as words } from './data/db.js';
-import { data as animals } from './data/animals.js';
-import { data as colors } from './data/colors.js';
-import { data as jokes } from './data/jokes.js';
-import { data as onomatopeias } from './data/onomatopeias.js';
-import { data as types } from './data/types.js';
+import { data as wordsData } from './data/db.js';
+import { data as animalsData } from './data/animals.js';
+import { data as colorsData } from './data/colors.js';
+import { data as jokesData } from './data/jokes.js';
+import { data as onomatopeiasData } from './data/onomatopeias.js';
+import { data as typesData } from './data/types.js';
+
+const words = wordsData.Palabras;
+const animals = animalsData.animals;
+const colors = colorsData.colors;
+const jokes = jokesData['spanish-jokes'];
+const onomatopeias = onomatopeiasData.onomatopeias;
+const types = typesData.types;
 
 export {
     words,
